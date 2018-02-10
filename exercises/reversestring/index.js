483e5cba052dbd4989b39f970f6f4edc15cb7d85
Eranus <ecthomps@umich.edu>
2018-02-10 10:24:24 -0500

    added solution for palindrome

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,27 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('');    
+    //debugger;
+    return str.split('').reduce((rev, char) =>
+        char + rev
+    , '')
 }
 
+//reverse('asdf');
+
 module.exports = reverse;
+
+
+// Solution 1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+//Solution 2
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let char of str)
+//         reversed = char + reversed;    
+//     return reversed;
+// }
